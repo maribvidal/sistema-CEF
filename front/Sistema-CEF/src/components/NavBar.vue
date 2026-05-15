326724800f8a662a788b5cbfcb0fd003dee6e73b
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2026-05-15 15:03:05 -0300

    Boton de registro añadido

--- front/Sistema-CEF/src/components/NavBar.vue
+++ front/Sistema-CEF/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app-bar app color="white" elevation="1">
         <!-- Logo Text / Image placeholder -->
-        <v-img class="NavBarIMG" :src="logoImg" alt="Logo CEF" contain height="40" />
+        <v-img class="NavBarIMG" :src="logoImg" alt="Logo CEF" contain height="40"/>
 
         <v-spacer></v-spacer>
 
@@ -10,6 +10,9 @@
             <v-btn variant="text" class="text-none text-subtitle-1 mx-1" color="black" to="/">Inicio</v-btn>
             <v-btn variant="text" class="text-none text-subtitle-1 mx-1" color="black" to="/inicioSesion">Iniciar Sesión</v-btn>
             <v-btn variant="text" class="text-none text-subtitle-1 mx-1" color="black" to="/sobre-nosotros">Nosotros</v-btn>
+            
+            <!-- Botón de Registro modificado -->
+            <v-btn variant="flat" color="red" class="text-none text-subtitle-1 ml-4 mr-2" to="/registro">Registrarse</v-btn>
         </div>
     </v-app-bar>
 </template>
@@ -29,5 +32,12 @@ export default {
 <style scoped>
 .NavBarIMG{
     margin-left: -50px;
+}
+.registerButton {
+    border: 2px solid black;
+    border-radius: 4px;
+    padding: 6px 12px;
+    transition: background-color 0.3s ease, color 0.3s ease;
+
 }
 </style>
