e59958389ceabffa2dbc7353660e4d696e2d7a5d
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2026-05-14 23:02:16 -0300

    SlideShow implementado con fotos de prueba

--- front/Sistema-CEF/src/views/HomeView.vue
+++ front/Sistema-CEF/src/views/HomeView.vue
@@ -1,22 +1,26 @@
 <template>
-  <v-container class="py-10">
-    <v-row justify="center">
-      <v-col cols="12" md="8" lg="6">
-        <v-card class="pa-6" rounded="lg" elevation="6">
-          <v-card-title class="text-h4">You did it!</v-card-title>
-          <v-card-subtitle>
-            Vuetify ya quedó integrado dentro de Sistema-CEF.
-          </v-card-subtitle>
+    <v-container class="home-view" fluid>
+        <v-containeer class="slideshow-container" fluid>
 
-          <v-card-text class="pt-4">
-            <p class="mb-4">
-              Esta es la pantalla inicial con componentes de Vuetify funcionando en la app
-              existente.
-            </p>
-            <v-btn color="primary" to="/inicioSesion">Ir a inicio de sesión</v-btn>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+        <SlideShow />
+        </v-containeer>
+    </v-container>
 </template>
+
+<script setup>
+import SlideShow from '@/components/SlideShow.vue'
+</script>
+
+<style scoped>
+.home-view {
+  
+}
+.slideshow-container {
+    position: inherit;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+</style>
