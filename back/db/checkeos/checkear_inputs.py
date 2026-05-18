4c18ea9a33d9c8e226defdee74fe3ceb22e14630
maribvidal <marianobazalarvidal@gmail.com>
2026-05-18 12:04:02 -0300

    Agregué un wrapper para validar las restricciones. Queda por probar

--- back/db/checkeos/checkear_inputs.py
+++ back/db/checkeos/checkear_inputs.py
@@ -1,3 +1,5 @@
+import functools, inspect
+
 def checkear_inputs(objetos):
     """Función que busca que los objetos recibidos
         cumplan las restricciones especificadas en
@@ -16,3 +18,26 @@ def checkear_inputs(objetos):
                     return result
 
     return {}
+
+# WRAPPERS
+
+def validar_inputs_db(func):
+    @functools.wraps(func) # Esto mantiene el nombre original y el docstring de la función
+    def wrapper(*args, **kwargs):
+        sig = inspect.signature(func)
+        bound_args = sig.bind(*args, **kwargs)
+        bound_args.apply_defaults()
+        
+        objetos_a_checkear = [
+            {"name": param_name, "value": param_value}
+            for param_name, param_value in bound_args.arguments.items()
+        ]
+        
+        errores = checkear_inputs(objetos_a_checkear)
+        if errores:
+            raise ValueError(f"Error de validación: {errores}")
+        return func(*args, **kwargs)
+        
+    return wrapper
+
+# TODO: Hacer un wrapper que escupa los errores que tira el motor de la BD
