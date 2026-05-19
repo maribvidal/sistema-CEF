07d65a3a01b81b70bc8350c221d48d86338b0af7
maribvidal <marianobazalarvidal@gmail.com>
2026-05-19 13:26:51 -0300

    Eliminé el archivo operaciones/modificar_db.py, y repartí las operaciones de modificación a la carpeta de cada entidad

--- back/db/operaciones/empleados/modificar_db.py
+++ back/db/operaciones/empleados/modificar_db.py
@@ -1,3 +1,16 @@
 from db.operaciones.conectar_db import conectarse_db
 from db.operaciones.commitear_db import commitear
 
+def actualizar_rol_empleado(
+    empleado_id: int,
+    nuevo_rol_id: int
+):
+    """Recibe el id de un empleado, y el id de un
+        rol, y le asigna al empleado un nuevo rol."""
+    cursor = conectarse_db()
+    cursor.execute("""
+        UPDATE Empleado
+        SET rol_id = ?
+        WHERE id = ?
+    """, (nuevo_rol_id, empleado_id))
+    commitear(cursor)
