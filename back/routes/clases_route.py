303a92945e12e7ed3690ea27048fe131cd62e66b
maribvidal <marianobazalarvidal@gmail.com>
2026-05-20 12:19:47 -0300

    Estuve agregando docstrings en clases_route.py

--- back/routes/clases_route.py
+++ back/routes/clases_route.py
@@ -6,6 +6,39 @@ clases_bp = Blueprint('clases', __name__)
  
 @clases_bp.route('/clases', methods=['GET'])
 def listar_clases():
+    """Este endpoint permite listar todas las clases disponibles 
+        en el sistema."""
     respuesta, status = listar_clases_service()
     
     return jsonify(respuesta), status
+
+@clases_bp.route('/clases', methods=['POST'])
+def publicar_clase():
+    """Este endpoint permite subir una nueva clase al
+        sistema. Los datos son recibidos en formato JSON."""
+
+    data = request.get_json()
+
+    # Usar clases_service.py
+
+def cancelar_clase():
+    """Este endpoint permite cancelar una clase específica. 
+        Recibe el ID de la clase a cancelar en formato JSON."""
+    data = request.get_json()
+
+    # Tengo que ver como se cancelaba una clase
+
+def eliminar_clase():
+    """Este endpoint permite eliminar una clase específica. 
+        Recibe el ID de la clase a eliminar en formato JSON."""
+    data = request.get_json()
+
+    # Por implementar
+
+def modificar_clase():
+    """Este endpoint permite modificar los detalles de una 
+        clase específica. Recibe el ID de la clase a modificar 
+        y los nuevos datos en formato JSON."""
+    data = request.get_json()
+
+    # Por implementar
