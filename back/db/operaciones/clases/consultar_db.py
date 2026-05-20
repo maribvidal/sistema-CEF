152c75ea79e547629b364767e1ee2cbbc47c1467
maribvidal <marianobazalarvidal@gmail.com>
2026-05-20 12:19:33 -0300

    Cambié la implementación de las operaciones de consulta de clases para que fuesen las mismas que estaban antes

--- back/db/operaciones/clases/consultar_db.py
+++ back/db/operaciones/clases/consultar_db.py
@@ -1,5 +1,17 @@
-from db.operaciones.exception_handler import ejecutar_fetchall, ejecutar_fetchone
+from db.operaciones.conectar_db import conectarse_db
 
 def listar_clases() -> list:
     """Hace una consulta para listar todas las clases, y devuelve una lista de tuplas"""
-    return ejecutar_fetchall("SELECT * FROM Clase")
+    cursor = conectarse_db()
+    res = cursor.execute("SELECT * FROM Clase")
+    res = res.fetchall()
+    cursor.connection.close()
+    return res
+
+def obtener_clase_por_id(clase_id: int) -> tuple:
+    """Hace una consulta para obtener una clase por su ID, y devuelve una tupla con los datos de la clase"""
+    cursor = conectarse_db()
+    res = cursor.execute("SELECT * FROM Clase WHERE id = ?", (clase_id,))
+    res = res.fetchone()
+    cursor.connection.close()
+    return res
