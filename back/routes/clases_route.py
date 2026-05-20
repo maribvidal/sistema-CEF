eb55250558a55692807cbde65a54d981cb65de02
maribvidal <marianobazalarvidal@gmail.com>
2026-05-20 13:48:03 -0300

    Agregué el endpoint para publicar_clase

--- back/routes/clases_route.py
+++ back/routes/clases_route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 
-from services.clases_service import ( listar_clases_service )
+from services.clases_service import listar_clases_service, publicar_clase_service
 
 clases_bp = Blueprint('clases', __name__)
  
@@ -19,14 +19,26 @@ def publicar_clase():
 
     data = request.get_json()
 
-    # Usar clases_service.py
+    estado = data.get("estado")
+    id_actividad = data.get("id_actividad")
+    id_profesor = data.get("id_profesor")
 
+    respuesta, status = publicar_clase_service(
+        estado,
+        id_actividad,
+        id_profesor
+    )
+
+    return jsonify(respuesta), status
+
+"""
 def cancelar_clase():
-    """Este endpoint permite cancelar una clase específica. 
-        Recibe el ID de la clase a cancelar en formato JSON."""
+    Este endpoint permite cancelar una clase específica. 
+        Recibe el ID de la clase a cancelar en formato JSON.
     data = request.get_json()
 
     # Tengo que ver como se cancelaba una clase
+"""
 
 def eliminar_clase():
     """Este endpoint permite eliminar una clase específica. 
--- back/services/clases_service.py
+++ back/services/clases_service.py
@@ -1,15 +1,43 @@
+from db.operaciones.conectar_db import conectarse_db
 from db.operaciones import listar_clases
+from db.operaciones_insercion_comunes import publicar_clase
 
 def listar_clases_service():
     """Service que lista las clases"""
-    clases = listar_clases()
+
+    cursor = conectarse_db()
+
+    clases = listar_clases(cursor)
     
     if not clases:
+        cursor.connection.close()
         return {
             "error": "No se encontraron clases"
         }, 404
         
-    # verificar si es que no tengo que hacer un for para agregar cada clase
+    cursor.connection.close()
     return {
         "clases": clases
     }, 200
+
+def publicar_clase_service(
+    estado: str,
+    id_actividad: int,
+    id_profesor: int
+):
+    """Service que publica una clase"""
+
+    cursor = conectarse_db()
+
+    respuesta = publicar_clase(cursor, estado, id_actividad, id_profesor)
+
+    if respuesta == -1:
+        cursor.connection.close()
+        return {
+            "error": "Error al publicar la clase."
+        }, 400
+    
+    cursor.connection.close()
+    return {
+        "mensaje": "Clase publicada exitosamente."
+    }
