8b0c4180fa7d3c61969595ab4155c31cd918b49b
maribvidal <marianobazalarvidal@gmail.com>
2026-05-20 12:10:06 -0300

    Agregué operaciones de inserción y de consulta un poco mas decoradas y que controlan lo que pasa internamente

---
+++ back/db/operaciones_consulta_comunes.py
@@ -0,0 +1,13 @@
+from db.operaciones.clases.consultar_db import obtener_clase_por_id
+
+# Estas operaciones devuelven una tupla o varias tuplas si hubo 
+# éxito, o None si hubo un error.
+
+def consultar_clase_por_id(clase_id: int) -> tuple:
+    """Función que se encarga de cnsultar una clase por su ID 
+        y controlar algunas cuestiones mas."""
+    try:
+        return obtener_clase_por_id(clase_id)
+    except Exception as e:
+        print(f"Error al consultar la clase: {e}")
+        return None
