8045da0e6dce2118a91222124ae129227901d308
maribvidal <marianobazalarvidal@gmail.com>
2026-05-20 11:57:54 -0300

    Modifiqué las operaciones de inserción de 'profesores', 'clases', y 'actividades', para que ahora retornen el número de id

--- back/db/operaciones/actividades/insertar_db.py
+++ back/db/operaciones/actividades/insertar_db.py
@@ -1,10 +1,12 @@
 from db.operaciones.conectar_db import conectarse_db
 from db.operaciones.commitear_db import commitear
 
-def insertar_actividad(nombre: str, precio_mensual: float):
-    """Permite insertar una fila para la tabla Actividad"""
+def insertar_actividad(nombre: str, precio_mensual: float) -> int:
+    """Permite insertar una fila para la tabla Actividad 
+        y retorna el ID de la fila insertada"""
     query = f"""INSERT INTO Actividad (nombre, precio_mensual)
                 VALUES ('{nombre}', {precio_mensual});"""
     cursor = conectarse_db()
     cursor.execute(query)
     commitear(cursor)
+    return cursor.lastrowid
--- back/db/operaciones/clases/insertar_db.py
+++ back/db/operaciones/clases/insertar_db.py
@@ -1,10 +1,12 @@
 from db.operaciones.conectar_db import conectarse_db
 from db.operaciones.commitear_db import commitear
 
-def insertar_clase(estado: str, actividad_id: int, profesor_id: int):
-    """Permite insertar una fila para la tabla Clase"""
+def insertar_clase(estado: str, actividad_id: int, profesor_id: int) -> int:
+    """Permite insertar una fila para la tabla Clase
+        y retorna el ID de la fila insertada"""
     query = f"""INSERT INTO Clase (estado, actividad_id, profesor_id)
                 VALUES ('{estado}', {actividad_id}, {profesor_id});"""
     cursor = conectarse_db()
     cursor.execute(query)
     commitear(cursor)
+    return cursor.lastrowid
