bef2366c06e51eab4ed95a634569caade360c5dc
maribvidal <marianobazalarvidal@gmail.com>
2026-05-20 13:00:47 -0300

    Hice los cambios necesarios en los archivos de operaciones comunes

--- back/db/operaciones_consulta_comunes.py
+++ back/db/operaciones_consulta_comunes.py
@@ -1,13 +1,15 @@
+import sqlite3 as sqlite
+
 from db.operaciones.clases.consultar_db import obtener_clase_por_id
 
 # Estas operaciones devuelven una tupla o varias tuplas si hubo 
 # éxito, o None si hubo un error.
 
-def consultar_clase_por_id(clase_id: int) -> tuple:
+def consultar_clase_por_id(cursor: sqlite.Cursor, clase_id: int) -> tuple:
     """Función que se encarga de cnsultar una clase por su ID 
         y controlar algunas cuestiones mas."""
     try:
-        return obtener_clase_por_id(clase_id)
+        return obtener_clase_por_id(cursor, clase_id)
     except Exception as e:
         print(f"Error al consultar la clase: {e}")
         return None
--- back/db/operaciones_insercion_comunes.py
+++ back/db/operaciones_insercion_comunes.py
@@ -1,3 +1,5 @@
+import sqlite3 as sqlite
+
 from db.operaciones.actividades.insertar_db import insertar_actividad
 from db.operaciones.clases.insertar_db import insertar_clase
 from db.operaciones.profesores.insertar_db import insertar_profesor
@@ -5,29 +7,29 @@ from db.operaciones.profesores.insertar_db import insertar_profesor
 # Estas operaciones devuelven el id de la fila insertada si hubo 
 # éxito, o -1 si hubo un error.
 
-def crear_actividad(nombre: str, precio_mensual: float) -> int:
+def crear_actividad(cursor: sqlite.Cursor, nombre: str, precio_mensual: float) -> int:
     """Función que se encarga de insertar una actividad y controlar
         algunas cuestiones mas."""
     try:
-        return insertar_actividad(nombre, precio_mensual)
+        return insertar_actividad(cursor, nombre, precio_mensual)
     except Exception as e:
         print(f" > Error al crear actividad: {e}")
         return -1
 
-def crear_profesor(nombre: str, apellido: str, genero: str, dni: int) -> int:
+def crear_profesor(cursor: sqlite.Cursor, nombre: str, apellido: str, genero: str, dni: int) -> int:
     """Función que se encarga de insertar un profesor y controlar
         algunas cuestiones mas."""
     try:
-        return insertar_profesor(nombre, apellido, genero, dni)
+        return insertar_profesor(cursor, nombre, apellido, genero, dni)
     except Exception as e:
         print(f" > Error al crear profesor: {e}")
         return -1
 
-def publicar_clase(estado: str, actividad_id: int, profesor_id: int) -> int:
+def publicar_clase(cursor: sqlite.Cursor, estado: str, actividad_id: int, profesor_id: int) -> int:
     """Función que se encarga de insertar una clase y controlar
         algunas cuestiones mas."""
     try:
-        return insertar_clase(estado, actividad_id, profesor_id)
+        return insertar_clase(cursor, estado, actividad_id, profesor_id)
     except Exception as e:
         print(f" > Error al publicar clase: {e}")
         return -1
