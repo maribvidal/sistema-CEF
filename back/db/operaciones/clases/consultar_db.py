e14216d02780f76c5c1215ba8e6c2ef776e445e6
SAugusS <augusto.lozada230063@alumnos.info.unlp.edu.ar>
2026-05-21 13:13:20 -0300

    nose

--- back/db/operaciones/clases/consultar_db.py
+++ back/db/operaciones/clases/consultar_db.py
@@ -4,6 +4,6 @@ def listar_clases(cursor) -> dict:
     """Hace una consulta para listar todas las clases, y devuelve una lista de tuplas"""
     return ejecutar_fetchall("SELECT * FROM Clase", cursor)
 
-def consultar_clase_por_id(clase_id, cursor) -> tuple:
+def consultar_clase_por_id(clase_id, cursor) -> dict:
     """Hace una consulta para obtener una clase por su ID, y devuelve una tupla con los datos de la clase"""
     return ejecutar_fetchone(f"SELECT * FROM Clase WHERE id = {clase_id}", cursor)
