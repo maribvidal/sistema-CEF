548e20ba8dd87329a883581fb8b7e0db163270d4
maribvidal <marianobazalarvidal@gmail.com>
2026-05-25 15:25:39 -0300

    Implementé la operación borrar_imagen_por_id

--- back/db/operaciones/imagenes/borrar_db.py
+++ back/db/operaciones/imagenes/borrar_db.py
@@ -2,4 +2,5 @@ from db.operaciones.exception_handler import ejecutar_query
 
 def borrar_imagen_por_id(id: int):
     """Permite borrar una fila de la tabla Imagen por su id"""
-    pass
+    query = f"DELETE FROM Imagen WHERE id = {id};"
+    return ejecutar_query(query)
