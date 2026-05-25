05367d719edf1e07d658e73030d7f210838a2fd9
maribvidal <marianobazalarvidal@gmail.com>
2026-05-25 15:47:11 -0300

    Agregué el parámetro cursor a las operaciones de imágenes

--- back/db/operaciones/imagenes/consultar_db.py
+++ back/db/operaciones/imagenes/consultar_db.py
@@ -1,6 +1,6 @@
-from db.operaciones.exception_handler import ejecutar_fetchall, ejecutar_fetchone
+from db.operaciones.exception_handler import ejecutar_fetchone
 
-def consultar_imagen_por_id(id: int):
+def consultar_imagen_por_id(id: int, cursor):
     """Permite consultar una fila de la tabla Imagen por su id"""
     query = f"SELECT * FROM Imagen WHERE id = {id};"
-    return ejecutar_fetchone(query)
+    return ejecutar_fetchone(query, cursor)
--- back/db/operaciones/imagenes/insertar_db.py
+++ back/db/operaciones/imagenes/insertar_db.py
@@ -1,6 +1,6 @@
 from db.operaciones.exception_handler import ejecutar_insertar
 
-def insertar_imagen(contenido: bytes):
+def insertar_imagen(contenido: bytes, cursor):
     """Permite insertar una fila para la tabla Imagen"""
     query = f"INSERT INTO Imagen (contenido) VALUES ('{contenido}');"
-    return ejecutar_insertar(query)
+    return ejecutar_insertar(query, cursor)
