3c875de71b31df3da5e980de9e29b45da78669db
maribvidal <marianobazalarvidal@gmail.com>
2026-05-25 15:24:09 -0300

    Implementé la operación insertar_imagen

--- back/db/operaciones/imagenes/insertar_db.py
+++ back/db/operaciones/imagenes/insertar_db.py
@@ -2,4 +2,5 @@ from db.operaciones.exception_handler import ejecutar_insertar
 
 def insertar_imagen(contenido: bytes):
     """Permite insertar una fila para la tabla Imagen"""
-    pass
+    query = f"INSERT INTO Imagen (contenido) VALUES ('{contenido}');"
+    return ejecutar_insertar(query)
