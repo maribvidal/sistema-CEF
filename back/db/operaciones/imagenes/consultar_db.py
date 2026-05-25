2f155efed6b8eb08ab62cf1dab893be5f72574ec
maribvidal <marianobazalarvidal@gmail.com>
2026-05-25 15:24:50 -0300

    Implementé la operación consultar_imagen_por_id

--- back/db/operaciones/imagenes/consultar_db.py
+++ back/db/operaciones/imagenes/consultar_db.py
@@ -2,4 +2,5 @@ from db.operaciones.exception_handler import ejecutar_fetchall, ejecutar_fetchon
 
 def consultar_imagen_por_id(id: int):
     """Permite consultar una fila de la tabla Imagen por su id"""
-    pass
+    query = f"SELECT * FROM Imagen WHERE id = {id};"
+    return ejecutar_fetchone(query)
