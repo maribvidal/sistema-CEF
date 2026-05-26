52e432818f9a440724861823f6b2131f6f2e79c1
maribvidal <marianobazalarvidal@gmail.com>
2026-05-26 13:38:00 -0300

    Cambié la implementación del endpont cambiar_permiso

--- back/routes/permisos_route.py
+++ back/routes/permisos_route.py
@@ -3,16 +3,15 @@ from services.permisos_service import cambiar_permiso_service
 
 permisos_bp = Blueprint('permisos', __name__)
 
-@permisos_bp.route('/permisos', methods=['POST'])
-def cambiar_permiso():
+@permisos_bp.route('/usuario/<int:usuario_id>/permisos', methods=['POST'])
+def cambiar_permiso(usuario_id):
     """Endpoint para cambiar el permiso de un usuario. 
         Recibe el DNI del usuario y el nuevo permiso en formato JSON."""
     data = request.get_json()
     
-    dni = data.get("dni")
     permiso = data.get("rol_id")
 
     # Acá se llama al servicio correspondiente para cambiar el permiso del usuario
-    respuesta, status = cambiar_permiso_service(dni, permiso)
+    respuesta, status = cambiar_permiso_service(usuario_id, permiso)
 
     return jsonify(respuesta), status
--- back/services/permisos_service.py
+++ back/services/permisos_service.py
@@ -1,14 +1,12 @@
 from db.operaciones.conectar_db import conectarse_db
 from db.operaciones.permisos.modificar_db import cambiar_permiso
 
-def cambiar_permiso_service(dni, permiso):
+def cambiar_permiso_service(id, permiso):
     """Función para cambiar el permiso de un usuario. 
-        Recibe el DNI del usuario y el nuevo permiso."""
+        Recibe el id del usuario y el nuevo permiso."""
     
     cursor = conectarse_db()
-    print("DNI recibido en el servicio: ", dni, " Permiso recibido en el servicio: ", permiso)
-    respuesta = cambiar_permiso(dni, permiso, cursor)
-    print("RESPUESTA DEL SERVICIO: ", respuesta)
+    respuesta = cambiar_permiso(id, permiso, cursor)
     cursor.connection.close()
 
     if respuesta['status'] == 'error':
@@ -18,5 +16,3 @@ def cambiar_permiso_service(dni, permiso):
         }, 500
 
     return respuesta['data'], 200
-
-
