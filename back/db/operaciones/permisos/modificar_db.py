fdbf3e22afa4995a2e26d4883255b0b0c9196930
maribvidal <marianobazalarvidal@gmail.com>
2026-05-26 13:35:31 -0300

    Cambié la forma en la que se realiza la operación cambiar_permiso: ahora, en vez de pedir el dni del usuario, pide el id

--- back/db/operaciones/permisos/modificar_db.py
+++ back/db/operaciones/permisos/modificar_db.py
@@ -1,16 +1,15 @@
 from db.operaciones.exception_handler import ejecutar_query
 
-
-def cambiar_permiso(dni, permiso, cursor):
+def cambiar_permiso(id, permiso, cursor):
     """Función para cambiar el permiso (rol) de un usuario.
-       Recibe el DNI del usuario y el nuevo `rol_id` y lo actualiza
+       Recibe el id del usuario y el nuevo `rol_id` y lo actualiza
        en la tabla `Usuario`.
     """
 
     query = f"""
         UPDATE Usuario
         SET rol_id = {permiso}
-        WHERE dni = {dni};
+        WHERE id = {id};
     """
 
     return ejecutar_query(query, cursor)
