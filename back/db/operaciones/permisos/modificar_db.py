3df7057d5f8cdedfb6c3e0fdcc902ef6191de158
SAugusS <augusto.lozada230063@alumnos.info.unlp.edu.ar>
2026-05-26 13:14:52 -0300

    Implementado HU cambiar permisos Se utiliza DNI y el rol_id para cambiar el rol del usuario. Se implementa un nuevo endpoint para realizar esta acción, y se modifica la base de datos Además, se actualizan los archivos necesarios para integrar esta funcionalidad en la aplicación.

--- back/db/operaciones/permisos/modificar_db.py
+++ back/db/operaciones/permisos/modificar_db.py
@@ -1,1 +1,16 @@
-from db.operaciones.exception_handler import ejecutar_query 
+from db.operaciones.exception_handler import ejecutar_query
+
+
+def cambiar_permiso(dni, permiso, cursor):
+    """Función para cambiar el permiso (rol) de un usuario.
+       Recibe el DNI del usuario y el nuevo `rol_id` y lo actualiza
+       en la tabla `Usuario`.
+    """
+
+    query = f"""
+        UPDATE Usuario
+        SET rol_id = {permiso}
+        WHERE dni = {dni};
+    """
+
+    return ejecutar_query(query, cursor)
