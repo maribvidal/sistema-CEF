b68678acac2fb6789ed7be073bc763e7d634af86
maribvidal <marianobazalarvidal@gmail.com>
2026-05-27 14:59:09 -0300

    Se terminó de implementar el endpoint crear_profesor junto al service correspondiente

--- back/services/profesores_service.py
+++ back/services/profesores_service.py
@@ -1,9 +1,32 @@
 from db.operaciones.conectar_db import conectarse_db
 from db.operaciones.profesores.consultar_db import listar_profesores
+from db.operaciones.profesores.insertar_db import insertar_profesor
 
 def listar_profesores_service():
-    """Service que lista los profesores"""
+    """Service que lista los profesores."""
     cursor = conectarse_db()
     respuesta = listar_profesores(cursor)
     cursor.connection.close()
     return respuesta, 200
+
+def crear_profesor_service(dni, nombre, apellido, genero):
+    """Service que crea un profesor."""
+
+    cursor = conectarse_db()
+
+    # Intentar insertar el nuevo profesor
+
+    res_insertar = insertar_profesor(nombre, apellido, genero, dni, cursor)
+
+    if res_insertar['status'] == 'error':
+        cursor.connection.close()
+        return {
+            "error": usuario['message']
+        }, 400
+
+    cursor.connection.commit()
+    cursor.connection.close()
+
+    return {
+        "mensaje": f"El profesor con id {res_insertar['data']} ha sido creado con éxito."
+    }, 200
