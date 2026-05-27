ff4d0b512b7c8d1a84ff2e85eee02e4552b4a48c
maribvidal <marianobazalarvidal@gmail.com>
2026-05-27 12:26:53 -0300

    Agregué que la operación consultar_clase_ocurrir_sala_por_claseid_fecha_hora devuelva el id en la tupla así puedo obtenerlo en el service de reservar_clase

--- back/db/operaciones/clase_ocurrir_sala/consultar_db.py
+++ back/db/operaciones/clase_ocurrir_sala/consultar_db.py
@@ -14,7 +14,7 @@ def consultar_clase_ocurrir_sala_por_claseid_fecha_hora(clase_id, fecha, hora, c
     """Función que consulta la clase_ocurrir_sala por el id de la clase, la fecha
         y la hora."""
     query = f"""
-        SELECT clase_id, sala_id, fecha, hora
+        SELECT id, clase_id, sala_id, fecha, hora
         FROM Clase_Ocurrir_Sala
         WHERE clase_id = {clase_id} AND fecha = '{fecha}' AND hora = '{hora}'
     """
