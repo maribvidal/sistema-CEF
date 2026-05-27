3bf7fdf3e84c9245aac9f0356d598be1a4545454
maribvidal <marianobazalarvidal@gmail.com>
2026-05-27 00:37:58 -0300

    Quité una operación que al final no implementé

--- back/db/operaciones/clases/consultar_db.py
+++ back/db/operaciones/clases/consultar_db.py
@@ -8,10 +8,3 @@ def listar_clases(cursor) -> dict:
 def consultar_clase_por_id(clase_id, cursor) -> dict:
     """Hace una consulta para obtener una clase por su ID, y devuelve una tupla con los datos de la clase"""
     return ejecutar_fetchone(f"SELECT * FROM Clase WHERE id = {clase_id}", cursor)
-
-def consultar_cupo_clase(clase_id, cursor) -> int:
-    """Hace una consulta para obtener el cupo de una clase por su ID, y devuelve un entero con el cupo"""
-    query = f"""
-                SELECT 
-    """
-    return ejecutar_fetchone(query, cursor)
