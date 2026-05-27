6e57777656728b49c4c6ec3861ffb71b6fa1a04a
maribvidal <marianobazalarvidal@gmail.com>
2026-05-27 11:52:16 -0300

    Agregué una operación de consulta para clase_ocurrir_sala

--- back/db/operaciones/clase_ocurrir_sala/consultar_db.py
+++ back/db/operaciones/clase_ocurrir_sala/consultar_db.py
@@ -9,3 +9,14 @@ def consultar_clase_ocurrir_sala_por_fecha_hora_sala(sala_id, fecha, hora, curso
     """
 
     return ejecutar_fetchone(query, cursor)
+
+def consultar_clase_ocurrir_sala_por_claseid_fecha_hora(clase_id, fecha, hora, cursor):
+    """Función que consulta la clase_ocurrir_sala por el id de la clase, la fecha
+        y la hora."""
+    query = f"""
+        SELECT clase_id, sala_id, fecha, hora
+        FROM Clase_Ocurrir_Sala
+        WHERE clase_id = {clase_id} AND fecha = '{fecha}' AND hora = '{hora}'
+    """
+
+    return ejecutar_fetchone(query, cursor)
