9bcae0b9bff590a8f09fd95a8bc375472382cd72
maribvidal <marianobazalarvidal@gmail.com>
2026-05-27 14:54:42 -0300

    Crée el endpoint crear_profesor. Ahora me queda terminar de implementarlo.

--- back/routes/profesores_route.py
+++ back/routes/profesores_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from services.profesores_service import listar_profesores_service
+from services.profesores_service import listar_profesores_service, crear_profesor_service
 
 profesores_bp = Blueprint('profesores', __name__)
 
@@ -8,4 +8,18 @@ def listar_profesores():
     """Endpoint para obtener la lista de profesores."""
     respuesta, status = listar_profesores_service()
     
+    return jsonify(respuesta), status
+
+@profesores_bp.route('/profesores', methods=['POST'])
+def crear_profesor():
+    """Endpoint para crear un profesor."""
+    data = request.get_json()
+
+    dni = data.get("dni")
+    nombre = data.get("nombre")
+    apellido = data.get("apellido")
+    genero = data.get("genero")
+
+    respuesta, status = crear_profesor_service(dni, nombre, apellido, genero)
+
     return jsonify(respuesta), status
