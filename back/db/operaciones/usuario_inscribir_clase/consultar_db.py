12c316894ece31e6b651e05d539d534df567ad39
maribvidal <marianobazalarvidal@gmail.com>
2026-05-27 00:38:42 -0300

    Agregué una operación llamada consultar_usuario_inscribir_clase_por_clase_ocurrir_sala_id. Con esta operación voy a poder obtener la cantidad de cupos disponibles que hay en una clase.

--- back/db/operaciones/usuario_inscribir_clase/consultar_db.py
+++ back/db/operaciones/usuario_inscribir_clase/consultar_db.py
@@ -4,3 +4,9 @@ def consultar_usuario_inscribir_clase_por_usuario_id(usuario_id: int, clase_id: 
     """Permite consultar si el usuario se ha inscrito en la clase, buscando por usuario_id y clase_id"""
     query = f"""SELECT * FROM Usuario_Inscribir_Clase WHERE usuario_id = {usuario_id} AND clase_id = {clase_id};"""
     return ejecutar_fetchone(query, cursor)
+
+def consultar_usuario_inscribir_clase_por_clase_ocurrir_sala_id(clase_ocurrir_sala_id: int, cursor):
+    """Permite obtener las filas de una clase en particular
+        a partir del clase_ocurrir_sala_id."""
+    query = f"""SELECT * FROM Usuario_Inscribir_Clase WHERE clase_ocurrir_sala_id = {clase_ocurrir_sala_id}"""
+    return ejecutar_fetchall(query, cursor)
