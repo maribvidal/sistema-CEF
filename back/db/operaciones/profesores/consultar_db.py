cb87d46fed566d145cc610b7ec8006710311d253
maribvidal <marianobazalarvidal@gmail.com>
2026-05-28 11:13:39 -0300

    Actualicé el modelo lógico de la base de datos. Adapté las operaciones de profesores para el nuevo modelo de la BD

--- back/db/operaciones/profesores/consultar_db.py
+++ back/db/operaciones/profesores/consultar_db.py
@@ -2,4 +2,4 @@ from db.operaciones.exception_handler import ejecutar_fetchall, ejecutar_fetchon
 
 def listar_profesores(cursor) -> dict:
     """Hace una consulta para listar todos los profesores, y devuelve una lista de tuplas"""
-    return ejecutar_fetchall("SELECT * FROM Profesor", cursor)
+    return ejecutar_fetchall("SELECT * FROM Usuario WHERE rol_id = 5", cursor)
