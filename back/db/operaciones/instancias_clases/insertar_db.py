edf62f2cedbc0ca4d080b0c1bad4e4ae93a2aa71
maribvidal <marianobazalarvidal@gmail.com>
2026-06-04 21:53:17 -0300

    Agregué una nueva operación: insertar_instancia_clase

--- back/db/operaciones/instancias_clases/insertar_db.py
+++ back/db/operaciones/instancias_clases/insertar_db.py
@@ -1,1 +1,9 @@
 from db.operaciones.exception_handler import ejecutar_insertar
+
+def insertar_instancia_clase(fecha, clase_id: int, cursor):
+    """ Permite insertar una instancia de una clase. """
+    query = f"""
+        INSERT INTO Instancia_Clase (fecha, clase_id)
+        VALUES                      ('{fecha}', {clase_id});
+    """
+    return ejecutar_insertar(query, cursor)
