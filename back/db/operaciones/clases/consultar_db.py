fe47e6a95c736c36787adfbf142a2507e37007f2
maribvidal <marianobazalarvidal@gmail.com>
2026-06-04 21:31:43 -0300

    Cambié la implementación de algunas operaciones de Clase, y borré alguna que otra

--- back/db/operaciones/clases/consultar_db.py
+++ back/db/operaciones/clases/consultar_db.py
@@ -5,25 +5,6 @@ def listar_clases(cursor) -> dict:
     return ejecutar_fetchall("""SELECT *
                                 FROM Clase""", cursor)
 
-def listar_clases_ocurriendo(cursor) -> dict:
-    """Hace una consulta para listar todas las clases, junto con la información obtenida
-        de la tabla Clase_Ocurrir_Sala, y devuelve una lista de tuplas."""
-    return ejecutar_fetchall("""SELECT c.id, c.estado, c.actividad_id, c.profesor_id, cos.dia, cos.hora, cos.sala_id, c.cupo_maximo
-                                FROM Clase c INNER JOIN Clase_Ocurrir_Sala cos ON c.id = cos.clase_id""", cursor)
-
 def consultar_clase_por_id(clase_id, cursor) -> dict:
     """Hace una consulta para obtener una clase por su ID, y devuelve una tupla con los datos de la clase"""
     return ejecutar_fetchone(f"SELECT * FROM Clase WHERE id = {clase_id}", cursor)
-
-def consultar_disponibilidad_clase(clase_id, cursor) -> dict:
-    """Hace una consulta para obtener la disponibilidad de una clase por su ID, y devuelve 1 si hay disponibilidad"""
-    query = f"""
-        SELECT 1
-        FROM Clase c
-        WHERE c.id = {clase_id} and c.cupo_maximo > (
-            SELECT COUNT(*) 
-            FROM Usuario_Inscribir_Clase 
-            WHERE clase_id = {clase_id}
-        )
-    """
-    return ejecutar_fetchone(query, cursor)
--- back/db/operaciones/clases/insertar_db.py
+++ back/db/operaciones/clases/insertar_db.py
@@ -1,7 +1,8 @@
 from db.operaciones.exception_handler import ejecutar_insertar
+from back.enums.dias import Dias
 
-def insertar_clase(estado: str, actividad_id: int, profesor_id: int, cupo_maximo: int, cursor):
+def insertar_clase(estado: str, actividad_id: int, profesor_id: int, sala_id: int, dia: Dias, hora: str, cupo_maximo: int, cursor):
     """Permite insertar una fila para la tabla Clase"""
-    query = f"""INSERT INTO Clase (estado, actividad_id, profesor_id, cupo_maximo)
-                VALUES ('{estado}', {actividad_id}, {profesor_id}, {cupo_maximo});"""
+    query = f"""INSERT INTO Clase (estado, actividad_id, profesor_id, sala_id, dia, hora, cupo_maximo)
+                VALUES ('{estado}', {actividad_id}, {profesor_id}, {sala_id}, {dia}, {hora}, {cupo_maximo});"""
     return ejecutar_insertar(query, cursor)
