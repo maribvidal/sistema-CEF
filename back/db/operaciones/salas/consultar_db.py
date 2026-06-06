6685815130512433510c732f76e21c0b6a048159
maribvidal <marianobazalarvidal@gmail.com>
2026-06-06 00:57:28 -0300

    Agregué la operación: consultar_sala_por_id

--- back/db/operaciones/salas/consultar_db.py
+++ back/db/operaciones/salas/consultar_db.py
@@ -1,5 +1,9 @@
 from db.operaciones.exception_handler import ejecutar_fetchall
 
+def consultar_sala_por_id(id: int, cursor) -> dict:
+    """Hace una consulta para devolver la tupla de una actividad por su id."""
+    return ejecutar_fetchone(f"SELECT * FROM Sala WHERE id = {id};", cursor)
+
 def listar_salas(cursor) -> dict:
     """Hace una consulta para listar todas las salas, y devuelve una lista de tuplas"""
     return ejecutar_fetchall("""SELECT s.id, s.nombre
