de95d7ba99ea53ea25f329632db30c9be7b94d2c
maribvidal <marianobazalarvidal@gmail.com>
2026-06-06 00:55:05 -0300

    Agregué la operación: consultar_actividad_por_id

--- back/db/operaciones/actividades/consultar_db.py
+++ back/db/operaciones/actividades/consultar_db.py
@@ -1,5 +1,9 @@
 from db.operaciones.exception_handler import ejecutar_fetchall, ejecutar_fetchone
 
+def consultar_actividad_por_id(id: int, cursor) -> dict:
+    """Hace una consulta para devolver la tupla de una actividad por su id."""
+    return ejecutar_fetchone(f"SELECT * FROM Actividad WHERE id = {id};", cursor)
+
 def listar_actividades(cursor) -> dict:
     """Hace una consulta para listar todas las actividades, y devuelve una lista de tuplas"""
     return ejecutar_fetchall("SELECT * FROM Actividad", cursor)
