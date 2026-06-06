8f49350e9f7bb8303c682d0885a555e26669d6e1
maribvidal <marianobazalarvidal@gmail.com>
2026-06-06 01:07:06 -0300

    Corregí el docstring de consultar_sala_por_id

--- back/db/operaciones/salas/consultar_db.py
+++ back/db/operaciones/salas/consultar_db.py
@@ -1,7 +1,7 @@
 from db.operaciones.exception_handler import ejecutar_fetchall
 
 def consultar_sala_por_id(id: int, cursor) -> dict:
-    """Hace una consulta para devolver la tupla de una actividad por su id."""
+    """Hace una consulta para devolver la tupla de una sala por su id."""
     return ejecutar_fetchone(f"SELECT * FROM Sala WHERE id = {id};", cursor)
 
 def listar_salas(cursor) -> dict:
