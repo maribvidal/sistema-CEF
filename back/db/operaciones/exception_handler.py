60d83bbed56abed1de2eca54fcffc597756db83b
maribvidal <marianobazalarvidal@gmail.com>
2026-06-08 09:38:03 -0300

    Modifiqué algunas funciones de exception_handler

--- back/db/operaciones/exception_handler.py
+++ back/db/operaciones/exception_handler.py
@@ -3,12 +3,14 @@ import sqlite3 as sqlite
 def ejecutar_fetchall(query, cursor):
     """Ejecuta una consulta SQL que devuelve varias filas y 
         maneja las excepciones."""
+    fallo = True
     try:
         cursor.execute(query)
         resultado = cursor.fetchall()
         # me tiraba error por el tipo de dato que devuelve el fetchall, así que lo convertí a una lista de diccionarios
         datos = [dict(fila) for fila in resultado]
         if resultado is None or str(resultado) == '[]':
+            fallo = False
             raise ValueError("Algo falló en la consulta (el resultado es None).")
 
         return {
@@ -17,16 +19,20 @@ def ejecutar_fetchall(query, cursor):
         }
 
     except Exception as e:
+        if (fallo):
+            print(f" >> Error: {str(e)}")
         return {
             "status": "error",
             "message": str(e)
         }
 
 def ejecutar_fetchone(query, cursor) -> dict:
+    fallo = True
     try:
         cursor.execute(query)
         resultado = cursor.fetchone()
         if resultado is None:
+            fallo = False
             raise ValueError("Algo falló en la consulta (el resultado es None).")
 
         resultado = dict(resultado)
@@ -37,6 +43,8 @@ def ejecutar_fetchone(query, cursor) -> dict:
         }
 
     except Exception as e:
+        if (fallo):
+            print(f" >> Error: {str(e)}")
         return {
             "status": "error",
             "message": str(e)
@@ -45,10 +53,12 @@ def ejecutar_fetchone(query, cursor) -> dict:
 def ejecutar_insertar(query, cursor):
     """Ejecuta una consulta SQL de inserción y maneja 
         las excepciones."""
+    fallo = True
     try:
         cursor.execute(query)
         nuevo_id = cursor.lastrowid
         if nuevo_id is None:
+            fallo = False
             raise ValueError("No se pudo insertar el registro.")
 
         cursor.connection.commit()
@@ -59,6 +69,8 @@ def ejecutar_insertar(query, cursor):
         }
 
     except Exception as e:
+        if (fallo):
+            print(f" >> Error: {str(e)}")
         return {
             "status": "error",
             "message": str(e)
