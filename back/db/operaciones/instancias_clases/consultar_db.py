79de268f01522d668727c910aaf70d401604bcf0
maribvidal <marianobazalarvidal@gmail.com>
2026-06-07 23:35:50 -0300

    Agregué una operación: consultar_instancia_clase_por_id

--- back/db/operaciones/instancias_clases/consultar_db.py
+++ back/db/operaciones/instancias_clases/consultar_db.py
@@ -1,1 +1,5 @@
 from db.operaciones.exception_handler import ejecutar_fetchone, ejecutar_fetchall
+
+def consultar_instancia_clase_por_id(ins_id, cursor) -> dict:
+    """Hace una consulta para obtener una instancia de clase por su ID, y devuelve una tupla."""
+    return ejecutar_fetchone(f"SELECT * FROM Instancia_Clase WHERE id = {ins_id}", cursor)
