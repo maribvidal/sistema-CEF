7c2c099047576cef79e3de135f229bfaa2e01c63
maribvidal <marianobazalarvidal@gmail.com>
2026-06-08 10:09:32 -0300

    Agregué la operación: insertar_cancelacion

--- back/db/operaciones/cancelaciones/insertar_db.py
+++ back/db/operaciones/cancelaciones/insertar_db.py
@@ -1,1 +1,8 @@
 from db.operaciones.exception_handler import ejecutar_insertar
+
+def insertar_cancelacion(reserva_id, cursor):
+    """Operación que permite insertar una cancelación."""
+    fecha = '2026-08-06'
+    query = f"""INSERT INTO Cancelacion (fecha, reserva_id)
+                                VALUES ('{fecha}', {reserva_id});"""
+    return ejecutar_insertar(query, cursor)
