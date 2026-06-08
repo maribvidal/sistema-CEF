21b49ba5271bbacd599f20d638bdb0ed5a0603f6
maribvidal <marianobazalarvidal@gmail.com>
2026-06-08 10:05:05 -0300

    Agregué una operación nueva: consultar_reserva_por_id

--- back/db/operaciones/reservas/consultar_db.py
+++ back/db/operaciones/reservas/consultar_db.py
@@ -1,1 +1,6 @@
 from db.operaciones.exception_handler import ejecutar_fetchone, ejecutar_fetchall
+
+def consultar_reserva_por_id(reserva_id: int, cursor):
+    """Operación que consulta por una reserva según su id y devuelve una tupla."""
+    query = f"SELECT * FROM Reserva WHERE id = {reserva_id}"
+    return ejecutar_fetchone(query, cursor)
