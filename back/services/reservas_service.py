05efdeaaa0705850de85f9880b340612a9946333
maribvidal <marianobazalarvidal@gmail.com>
2026-06-09 13:35:02 -0300

    Cambié el cancelar_reserva

--- back/services/reservas_service.py
+++ back/services/reservas_service.py
@@ -1,6 +1,7 @@
 from db.operaciones.conectar_db import conectarse_db
 from db.operaciones.reservas.consultar_db import consultar_reserva_por_id
 from db.operaciones.cancelaciones.insertar_db import insertar_cancelacion
+from db.operaciones.reservas.borrar_db import borrar_reserva
 
 def _msj_error_helper(razon: str, cursor):
     cursor.connection.close()
@@ -31,7 +32,9 @@ def cancelar_reserva_service(reserva_id):
     # Validar si la reserva existe
     res_reserva = consultar_reserva_por_id(reserva_id, cursor)
     if res_reserva['status'] == 'error':
-        return _msj_error_helper(res_reserva['message']), 400
+        return _msj_error_helper(res_reserva['message'], cursor), 400
+    if res_reserva['status'] == 'success' and res_reserva['data'] is None:
+        return _msj_error_helper(f"La reserva con el id {reserva_id} no existe.", cursor), 401
 
     # Cancelar reserva (insertar una cancelación)
     res_elim_reserva = borrar_reserva(reserva_id, cursor)
@@ -39,7 +42,7 @@ def cancelar_reserva_service(reserva_id):
     id_ins_clase = res_reserva["data"]["inst_clase_id"]
     res_ins_cancelacion = insertar_cancelacion(id_usuario, id_ins_clase, cursor)
     if res_ins_cancelacion['status'] == 'error':
-        return _msj_error_helper(res_ins_cancelacion['message']), 401
+        return _msj_error_helper(res_ins_cancelacion['message']), 402
     
     cursor.connection.commit()
     
