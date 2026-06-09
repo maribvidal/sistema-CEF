9bacd30f85a912e46e4881593479b8fc3a8d3aac
maribvidal <marianobazalarvidal@gmail.com>
2026-06-09 00:09:03 -0300

    Agregué la operación: obtener_reservas_usuario_dia_hora

--- back/db/operaciones/reservas/consultar_db.py
+++ back/db/operaciones/reservas/consultar_db.py
@@ -1,6 +1,17 @@
 from db.operaciones.exception_handler import ejecutar_fetchone, ejecutar_fetchall
+from enums.dias import Dias
 
 def consultar_reserva_por_id(reserva_id: int, cursor):
     """Operación que consulta por una reserva según su id y devuelve una tupla."""
     query = f"SELECT * FROM Reserva WHERE id = {reserva_id}"
     return ejecutar_fetchone(query, cursor)
+
+def obtener_reservas_usuario_dia_hora(id_usuario: int, dia: Dias, hora: str, cursor)
+    """Operación que consulta por reservas según el id del usuario, y el dia y hora
+        de la instancia de la clase asociada con la reserva."""
+    query = f"""SELECT r.usuario_id, r.inst_clase_id, r.fecha
+                FROM Reserva r INNER JOIN Instancia_Clase ic ON (r.inst_clase_id = ic.id)
+                                INNER JOIN Clase c ON (ic.clase_id = c.id)
+                WHERE r.usuario_id = {id_usuario} AND c.dia = '{dia}' AND c.hora = '{hora}';"""
+    return ejecutar_fetchall(query, cursor)
+ 
