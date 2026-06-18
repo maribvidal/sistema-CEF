8e9722f356e03a8619f6944cf2b2627b8a15d6f9
maribvidal <marianobazalarvidal@gmail.com>
2026-06-17 23:49:30 -0300

    Agregué una nueva carpeta para las operaciones hechas a la tabla Usuario_Pertenece_Lista_Espera_Abonados, y agregué la operación: borrar_usuario_pertenece_lista_espera_abonados_por_id

---
+++ back/db/operaciones/usuario_pertenece_lista_espera_abonados/borrar_db.py
@@ -0,0 +1,8 @@
+from db.operaciones.exception_handler import ejecutar_query
+
+def borrar_usuario_pertenece_lista_espera_abonados_por_id(id_lea: int, id_usuario: int, cursor):
+    """Operación que elimina de una lista de espera, a un usuario
+        que pertenecía a esa lista."""
+    query = f"""DELETE FROM Usuario_Pertenece_Lista_Espera_Abonados
+                WHERE lea_id = {id_lea} AND id_usuario = {id_usuario};"""
+    ejecutar_query(query, cursor)
