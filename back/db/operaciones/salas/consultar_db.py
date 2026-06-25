75ed359416881f3c1ae5d20a914fa2d380ac5f2b
maribvidal <marianobazalarvidal@gmail.com>
2026-06-25 15:31:12 -0300

    Agregué operacion: consultar_sala_por_dia_hora

--- back/db/operaciones/salas/consultar_db.py
+++ back/db/operaciones/salas/consultar_db.py
@@ -8,3 +8,11 @@ def listar_salas(cursor) -> dict:
     """Hace una consulta para listar todas las salas, y devuelve una lista de tuplas"""
     return ejecutar_fetchall("""SELECT s.id, s.nombre
                                 FROM Sala s""", cursor)
+
+def consultar_sala_por_dia_hora(dia: str, hora: str, cursor) -> dict:
+    """Hace una consulta para devolver la tupla de una sala por el día y hora
+        en el que transcurra una clase.."""
+    return ejecutar_fetchone(f"""SELECT s.id, s.nombre
+                                FROM Sala s
+                                JOIN Clase c ON s.id = c.sala_id
+                                WHERE c.dia = '{dia}' AND c.hora = '{hora}';""", cursor)
