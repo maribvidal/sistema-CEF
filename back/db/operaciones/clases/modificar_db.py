e917f2332b80f3be2de659de29364d9bce585982
maribvidal <marianobazalarvidal@gmail.com>
2026-06-29 18:35:31 -0300

    Actualicé la operación modificar_clase para que se pueda modificar el monto de una clase, en el caso de que se haya pasado este

--- back/db/operaciones/clases/modificar_db.py
+++ back/db/operaciones/clases/modificar_db.py
@@ -7,11 +7,13 @@ def modificar_clase_estado(clase_id: int, estado: str, cursor):
                 WHERE id = {clase_id};"""
     return ejecutar_query(query, cursor)
 
-def modificar_clase(clase_id: int, estado: str, id_profesor: int, sala_id: int, cursor):
+def modificar_clase(clase_id: int, estado: str, id_profesor: int, sala_id: int, cursor, monto = None):
     """Permite modificar los detalles de una clase."""
     query = f"""UPDATE Clase
                 SET estado = '{estado}',
-                    profesor_id = {id_profesor},
-                    sala_id = {sala_id}
+                    profesor_id = {id_profesor},"""
+    if monto:
+        query += f"monto = {monto},"
+    query += f"""sala_id = {sala_id}
                 WHERE id = {clase_id};"""
     return ejecutar_query(query, cursor)
