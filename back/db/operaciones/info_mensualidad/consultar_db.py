07f44abc4723c501bd9176e986cf6309fab9102f
maribvidal <marianobazalarvidal@gmail.com>
2026-07-02 13:09:01 -0300

    Agregué: consultar_info_mensualidad

--- back/db/operaciones/info_mensualidad/consultar_db.py
+++ back/db/operaciones/info_mensualidad/consultar_db.py
@@ -8,3 +8,11 @@ def comprobar_existe_info_mensualidad(id_mensualidad: int, cursor) -> dict:
         WHERE mensualidad_id = {id_mensualidad}
     """
     return ejecutar_fetchone(query, cursor)
+
+def consultar_info_mensualidad(id_inf: int, cursor) -> dict:
+    query = f"""
+        SELECT *
+        FROM Info_Mensualidad
+        WHERE id = {id_inf}
+    """
+    return ejecutar_fetchone(query, cursor)
